27a3058a32ee9d13b17a2cccc7381d068955947a
Joseph <josembi@gmail.com>
2017-11-18 16:04:40 +0100

    Add New & Create Actions with Views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,32 @@ class MessagesController < ApplicationController
 	def index 
 		
 	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(josembi)
+
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def josembi
+		params.require(:message).permit(:title, :description)
+	end
 end
