dc4ec1f7f064fa2a32a11abb3eab4aa3b47ec1e9
Joseph <josembi@gmail.com>
2017-11-19 21:08:43 +0100

    Edit & Delete Actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+	before_action :mugabe_went, only: [:show, :edit, :update, :destroy]
+
 	def index 
 		@messages = Message.all.order("created_at DESC")
 	end
 
 	def show
-		@message = Message.find(params[:id])
 	end
 
 	def new
@@ -15,21 +16,36 @@ class MessagesController < ApplicationController
 		@message = Message.new(josembi)
 
 		if @message.save
+			flash[:success] = "Message saved Successfully"
 			redirect_to root_path
 		else
 			render 'new'
 		end
 	end
 
+	def edit
+	end
+
 	def update
-		
+		if @message.update(josembi)
+			flash[:success] = "Message successfully updated!"
+			redirect_to message_path 
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-		
+		@message.destroy
+		flash[:danger] = "Message successfully destroyed"
+		redirect_to root_path
 	end
 
 	private
+
+	def mugabe_went
+		@message = Message.find(params[:id])
+	end
 
 	def josembi
 		params.require(:message).permit(:title, :description)
